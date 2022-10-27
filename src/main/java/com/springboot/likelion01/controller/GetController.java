3c37ca240e57ed28ba636a0eaae32f5a09b8dec7
S2ujeong <sj980204@gmail.com>
2022-10-27 14:10:25 +0900

    1. GetMapping 다양한 활용 2. Dto 만들어서 객체로 연결

--- src/main/java/com/springboot/likelion01/controller/GetController.java
+++ src/main/java/com/springboot/likelion01/controller/GetController.java
@@ -1,11 +1,13 @@
 package com.springboot.likelion01.controller;
 
-import org.springframework.lang.UsesSunHttpServer;
+import com.springboot.likelion01.domain.dto.MemberDto;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
-public class HelloController {
+public class GetController {
 
     // http://localhost:8081/api/v1/get-api/hello
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
@@ -41,5 +43,21 @@ public class HelloController {
     ) {
         return name+ " " + email + " " + organization;
     }
+
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param) {
+        StringBuilder sb = new StringBuilder();
+        param.entrySet().forEach( map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+        return sb.toString();
+    }
+
+    // MemberDto 객체 활용하여 코드 가독성 높이기
+    @GetMapping(value = "/request3")
+    public String getRequestParam3(MemberDto memberDto) {
+        return memberDto.toString();
+    }
+
 
 }
---
+++ src/main/java/com/springboot/likelion01/domain/dto/MemberDto.java
@@ -0,0 +1,28 @@
+package com.springboot.likelion01.domain.dto;
+
+public class MemberDto {
+    private String name;
+    private String email;
+    private String organization;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberDto{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", organization='" + organization + '\'' +
+                '}';
+    }
+}
