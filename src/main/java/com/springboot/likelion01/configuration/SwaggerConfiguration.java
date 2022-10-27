b1c53d616dd9d9a5cc56bbf96113d416185d74db
S2ujeong <sj980204@gmail.com>
2022-10-27 16:01:38 +0900

    Swagger 설치 ( 오류해결 )

--- src/main/java/com/springboot/likelion01/configuration/SwaggerConfiguration.java
+++ src/main/java/com/springboot/likelion01/configuration/SwaggerConfiguration.java
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
-
+    // http://localhost:8081/swagger-ui/
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
